24c9f50ba09dd0704bc951d81863dd6a9bc4d6b5
Emanuel Rey Reboredo <emanureyreboredo@gmail.com>
2024-10-30 19:44:50 -0300

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    mode:"development",
+    optimization:{
+        minimizer:[new CssMinimizerPlugin()]
+    },
+    module:{
+        rules:[
+            {
+                test:/\.html$/,
+                use:
+                [
+                    {
+                        loader: "html-loader",
+                        options:{minimize:false}
+                    },
+                    
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude:/styles\.css$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            ignoreOrder:false
+        }),
+        new CopyPlugin({
+            patterns:[
+               {from:"src/assets",to:"assets/"}
+            ]
+        })
+    ],
+    output: {
+        clean: true, // Clean the output directory before emit.
+    },
+}
